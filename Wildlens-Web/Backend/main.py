b5ec84d243b9a63b215efcb23d4069d378826a12
yacucdeptrai <122144376+yacucdeptrai@users.noreply.github.com>
2025-12-08 05:23:18 +0700

    button

--- Wildlens-Web/Backend/main.py
+++ Wildlens-Web/Backend/main.py
@@ -472,4 +472,4 @@ def get_history(record_id: str):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="127.0.0.1", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
